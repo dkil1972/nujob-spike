4762e387540216a4139d2415e77289f50bb94fb9
Dermot <administration@invoko.co.uk>
2013-09-26 06:24:40 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NujobSpike::Application.config.secret_key_base = '23db997e0baf441d31c9f680a1091f75a73a787567678e8b5c743c3f2a7d2e6bfeb9a002b2aa17907bd92df473366f2c9ebc7422d0231eef14a00e4e25929f9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NujobSpike::Application.config.session_store :cookie_store, key: '_nujob-spike_session'
